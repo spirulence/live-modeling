49485baa8cbce0fe5bbacf5be564ad2b73f7d740
spirulence <pannewb@gmail.com>
2018-03-13 22:07:54 -0700

    add simple smoke test

---
+++ backend/tests/smoke.py
@@ -0,0 +1,14 @@
+import unittest
+
+from backend.tasks import node_step
+from backend.simulation_objects import NodeState
+
+class SmokeTest(unittest.TestCase):
+
+    def test_node_step(self):
+        state = NodeState()
+
+        result = node_step.delay(state)
+        next_state = result.get(timeout=1)
+
+        self.assertEqual(state.step_number + 1, next_state.step_number)
