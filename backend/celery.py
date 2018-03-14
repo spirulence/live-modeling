b20708819f917e2f693864b5508ff154ad8e42c3
spirulence <pannewb@gmail.com>
2018-03-13 22:14:21 -0700

    update conf to pass smoke test, switch to rpc results backend

--- backend/celery.py
+++ backend/celery.py
@@ -2,13 +2,13 @@ from celery import Celery
 
 app = Celery('backend',
              broker='amqp://',
-             backend='amqp://',
+             backend='rpc://',
              include=['backend.tasks'])
 
 app.conf.update(
     result_expires=3600,
     task_serializer='pickle',
-    accept_content=['application/x-python-serialize']
+    accept_content=['application/json', 'application/x-python-serialize']
 )
 
 if __name__ == '__main__':
