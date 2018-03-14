cc80da05776a4466e54d45d05c39920834588bdc
spirulence <pannewb@gmail.com>
2018-03-13 22:11:11 -0700

    accept_content takes a list, duh

--- backend/celery.py
+++ backend/celery.py
@@ -8,7 +8,7 @@ app = Celery('backend',
 app.conf.update(
     result_expires=3600,
     task_serializer='pickle',
-    accept_content='application/x-python-serialize'
+    accept_content=['application/x-python-serialize']
 )
 
 if __name__ == '__main__':
